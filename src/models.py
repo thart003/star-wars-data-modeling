c89a132495149adef5c7f199d85dd5fbd3e12dc8
Taylor Hart <thart003@ucr.edu>
2021-12-10 00:58:48 +0000

    initial commit

--- src/models.py
+++ src/models.py
@@ -29,5 +29,59 @@ class Address(Base):
     def to_dict(self):
         return {}
 
+
+class People(Base):
+    __tablename__ = 'people'
+    id = Column(Integer, primary_key=True)
+    homeplanet_id = Column(String(250))
+    name = Column(String(250))
+    birth_year = Column(Integer, primary_key=True)
+    eye_color = Column(String(250))
+    gender = Column(String(250))
+    hair_color = Column(String(250))
+    height = Column(Integer, primary_key=True)
+    mass = Column(Integer, primary_key=True)
+    skin_color = Column(Integer, primary_key=True)
+
+class Planets(Base):
+    __tablename__ = 'planets'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250))
+    population = Column(Integer, primary_key=True)
+    climate = Column(String(250))
+    diameter = Column(Integer, primary_key=True)
+    gravity = Column(String(250))
+    rotation_period = Column(Integer, primary_key=True)
+    surface_water = Column(Integer, primary_key=True)
+    terrain = Column(String(250))
+
+class Vehicles(Base):
+    __tablename__ = 'vehicles'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250))
+    cargo_capacity = Column(Integer, primary_key=True)
+    consumables = Column(String(250))
+    cost_in_credits = Column(Integer, primary_key=True)
+    crew = Column(Integer, primary_key=True)
+    length = Column(Integer, primary_key=True)
+    manufacturer = Column(String(250))
+    passengers = Column(Integer, primary_key=True)
+
+class Users(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    username = Column(String(250))
+    firstname = Column(String(250))
+    lastname = Column(String(250))
+    email = Column(String(250))
+
+class Drives(Base):
+    __tablename__ = 'drives'
+    people_id = Column(Integer, ForeignKey('people.id'), primary_key=True)
+    vehicle_id = Column(Integer, ForeignKey('vehicle.id'), primary_key=True)
+    
+
+
+
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
